95a255f216cdf074d974e870401c918a97227877
zeuce <70825531+zeuce@users.noreply.github.com>
2021-05-07 07:05:15 -0700

    added some error functions

--- error.go
+++ error.go
@@ -1,8 +1,34 @@
 package gotils
 
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type httpResponse struct {
+	StatusCode int `json:"statusCode"`
+	Message string `json:"message"`
+}
+
 //Check will log output to console on error
 func Check(err error) {
 	if(err != nil){
 		panic(err)
 	}
 } 
+
+// SendError - writes error message to io.Writer
+func SendError(err error, w io.Writer) {
+	w.Write([]byte(err.Error()))
+}
+
+// SendHTTPError - sends 500 error with http writer
+func SendHTTPError(err error, w http.ResponseWriter) {
+	d := httpResponse{
+		StatusCode: 500,
+		Message: err.Error(),
+	}
+
+	json.NewEncoder(w).Encode(d)
+}
